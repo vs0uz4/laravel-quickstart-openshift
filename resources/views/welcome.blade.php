b392a0489f1ad775306d3a438d970903c47c4b73
vS0uz4 <vitor.rodrigues@gmail.com>
2015-10-22 19:38:40 -0200

    change welcome view and setting +x permission for pre_build and pre_restart files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5.1</div>
+                <div class="quote">Quickstart for Openshift</div>
             </div>
         </div>
     </body>
